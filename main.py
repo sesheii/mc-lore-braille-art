90de2e1f56c954b9499db1fea1ba043b538eaa12
FalconAI <61083677+bogdied@users.noreply.github.com>
2024-01-25 23:28:05 +0200

    no gui

---
+++ main.py
@@ -0,0 +1,39 @@
+import nbtlib
+import subprocess
+from tkinter import filedialog as fd
+from nbtlib import String, Path
+import os
+
+
+def open_file_selection():
+    filename = fd.askopenfilename()
+    return filename
+
+
+nbt_file_path = 'D:\\Minecraft\\game\\hotbar.nbt' # path to hotbar.nbt in minecraft directory
+nbt_file = nbtlib.load(nbt_file_path)
+
+row_num = 4 # row number in "Saved Toolbars" tab, starts with 0
+col_num = 7 # column number in "Saved Toolbars" tab, starts with 0
+
+image_path = open_file_selection().replace('/', '\\')
+print(image_path)
+
+# uncomment one of these to change what the result looks like
+
+# subprocess.call(f'ascii-image-converter.exe {image_path} -H 30 -b --threshold 150 --save-txt .\\txt-images')
+# subprocess.call(f'ascii-image-converter.exe {image_path} -H 30 -b --dither --save-txt .\\txt-images')
+# subprocess.call(f'ascii-image-converter.exe {image_path} -H 20 -b --threshold 210 --save-txt .\\txt-images')
+subprocess.call(f'ascii-image-converter.exe {image_path} -H 20 -b --dither --save-txt .\\txt-images')   
+
+filename, extension = os.path.splitext(os.path.basename(image_path))
+image_file_name = f'.\\txt-images\\{filename}-ascii-art.txt'
+
+my_compound = []
+with open(image_file_name, 'r') as f:
+    for row in f:
+        my_compound.append(String('§l§7' + row.strip()))
+
+lore_list_tag = nbtlib.List(my_compound)
+nbt_file[Path(f'{row_num}[{col_num}].tag.display.Lore')] = lore_list_tag
+nbt_file.save(nbt_file_path)
